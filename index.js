e1703b53d3d82e0350a33a51d5c3c2295c337b22
bekve <bekve@users.noreply.github.com>
2025-11-21 23:08:10 +0700

    fix port for railway

--- index.js
+++ index.js
@@ -38,6 +38,7 @@ app.post("/generate-pdf", (req, res) => {
 });
 
 // ---- Start Server ----
-app.listen(3000, () => {
-    console.log("Server running on port 3000");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log("Server running on port", PORT);
 });
